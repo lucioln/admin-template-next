b1fdf6f962e4ddee6bef2ab870aff9dabf2852e4
Lucio Noleto <luciolnoleto@gmail.com>
2023-06-26 15:16:11 -0300

    Criando componentes de layout

---
+++ src/components/template/MenuLateral.tsx
@@ -0,0 +1,11 @@
+interface MenuLateralProps{
+
+}
+
+export default function MenuLateral(props:MenuLateralProps){
+    return (
+        <div>
+            <h1>Menu</h1>
+        </div>
+    )
+}
---
+++ src/components/template/Titulo.tsx
@@ -0,0 +1,13 @@
+interface TituloProps{
+    titulo: string
+    subtitulo: string
+}
+
+export default function Titulo(props:TituloProps){
+    return (
+        <div>
+            <h1>{props.titulo}</h1>
+            <h2>{props.subtitulo}</h2>
+        </div>
+    )
+}
