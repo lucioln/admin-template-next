78d5d54becb68af58f81944ff78a619f5df83ede
Lucio Noleto <luciolnoleto@gmail.com>
2023-06-26 17:26:15 -0300

    Introduzindo Context API para fazer o swith de tema

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,22 +1,37 @@
 import Link from "next/link"
 
 interface MenuItemProps {
-    url: string
+    url?: string
     texto: string
     icon: any
+    className?: string
+    onClick?: (evento: any) => void 
 }
 
 export default function MenuItem(props: MenuItemProps) {
-    return (
-        <li className={`hover:bg-blue-100`}>
-            <Link href={props.url}>
-                <p className={`flex flex-col justify-center items-center h-20 w-20`}>
+
+    function renderizarItem() {
+        return (
+            <p className={`flex flex-col justify-center items-center h-20 w-20`}>
                 {props.icon}
-                <span className={`text-xs font-light text-gray-600`}>
+                <span className={`text-xs font-light `}>
                     {props.texto}
                 </span>
-                </p>
-            </Link>
+            </p>
+        )
+    }
+
+    return (
+        <li onClick={props.onClick} className={`cursor-pointer text-gray-600 hover:bg-blue-100  
+        dark:text-gray-200 dark:hover:bg-blue-700 ${props.className}`}>
+            {props.url ? (
+                <Link href={props.url}>
+                   {renderizarItem()}
+                </Link>
+            ) : (
+                renderizarItem()
+            )}
         </li>
+
     )
 }
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,13 +1,25 @@
-import { IconConfigs, IconHome, IconNotifications } from "../icons"
+import { IconConfigs, IconHome, IconLogout, IconNotifications } from "../icons"
 import MenuItem from "./MenuItem"
+import Logo from "./Logo"
 
-export default function MenuLateral(){
+export default function MenuLateral() {
     return (
-        <aside>
-            <ul>
-                <MenuItem url="/" texto="Home" icon={IconHome}/>
+        <aside className={`flex flex-col bg-gray-200 text-gray-900
+                dark:bg-gray-900 dark:text-gray-200`}>
+            <div className={`
+            flex flex-col items-center justify-center 
+            h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800`}>
+                <Logo />
+            </div>
+            <ul className={`flex-grow `}>
+                <MenuItem url="/" texto="Home" icon={IconHome} />
                 <MenuItem url="/ajustes" texto="Ajustes" icon={IconConfigs} />
                 <MenuItem url="/notificacoes" texto="Notificações" icon={IconNotifications} />
+            </ul>
+            <ul>
+                <MenuItem texto="Sair" icon={IconLogout} onClick={()=>{alert('Saiu')}} 
+                className={`hover:bg-red-600 dark:bg-red-700 dark:hover:bg-red-900
+                            hover:text-white`}/>
             </ul>
         </aside>
     )
